0d0b75d94bc1640f80ee0e7de2d089313c083db3
Alan-MOK <50497802+Alan-MOK@users.noreply.github.com>
2023-10-18 16:04:12 +0800

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import plotly.express as px
-import math
+# import math
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 
